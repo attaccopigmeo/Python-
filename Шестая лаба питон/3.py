582030ba51b2ba598043ba82c2c6ec52eddfde2f
attaccopigmeo <camillaschmidt@yandex.ru>
2024-11-12 08:07:53 +0500

    лаба 6

--- Шестая лаба питон/3.py
+++ Шестая лаба питон/3.py
@@ -1,4 +1,4 @@
-def check(num_str, p):
+def check(num_str, p): # Проверяем, соответствует ли число p-ной системы
     for digit in num_str:
         # В системе счисл с осн p могут быть цифры только меньше p
         if int(digit) >= p:
@@ -6,11 +6,11 @@ def check(num_str, p):
     return True
 
 
-def ten_in_p_f(dec_part, p):
+def ten_in_p_f(dec_part, p): # Переводим дробную часть из десятичной в p-ную
     res = 0
     n = 0.1 # множитель для десятич. ф-мы
     cnt = 0
-    while dec_part > 0 and cnt < 4:
+    while dec_part > 0 and cnt < 4: # С точностью до 4-х знаков переводим
         dec_part *= p
         digit = int(dec_part)
         res += digit * n
@@ -18,9 +18,9 @@ def ten_in_p_f(dec_part, p):
         dec_part -= digit
         cnt += 1
     return res
-# Переводим дробную часть из десятичной в p-ную
 
-def ten_in_p(int_part, p):
+
+def ten_in_p(int_part, p): # Переводим целую часть из десятичной в p-ную
     res = 0
     place = 1 #позиция в числе
     while int_part > 0:
@@ -30,7 +30,7 @@ def ten_in_p(int_part, p):
     return res
 
 
-def p_in_10(number, p):
+def p_in_10(number, p): # Перевод числа из p-ной в десятичную
     int_part = int(number)
     dec_part = number - int_part
     # Перевод цел. части
@@ -54,11 +54,11 @@ def p_in_10(number, p):
     return res_int + res_dec
 
 
-def calculate(x, y, z, p):
+def calculate(x, y, z, p): # Вычисление выражения
     x_10 = p_in_10(x, p)
     y_10 = p_in_10(y, p)
     z_10 = p_in_10(z, p)
-    if z_10 == 0:
+    if z_10 == 0: # Проверка во избежание нуля в знаменателе
         return None
     res_10 = (x_10 + y_10) / z_10
     int_part = int(res_10)
@@ -77,6 +77,26 @@ if check(str(int(x)), p) == False or check(str(int(y)), p) == False or check(str
 else: 
     res = calculate(x, y, z, p)
     if res is not None:
-        print(res)
+        print("Результат в системе счисления c осн.", p, ":", res)
     else:
         print("Error")
+# Тесты
+# Введите осн. сист. счисл.: 2
+# Введите число x: 1010
+# Введите число y: 101
+# Введите число z: 10
+# Результат в системе счисления c осн. 2 : 111.1
+
+# Введите осн. сист. счисл.: 3
+# Введите число x: 2212210
+# Введите число y: 1000121
+# Введите число z: 3
+# Error
+
+# Введите осн. сист. счисл.: 5
+# Введите число x: 10
+# Введите число y: 2
+# Введите число z: 10
+# Результат в системе счисления c осн. 5 : 1.1444
+# Результат неточный, тк влияет объем выделенной памяти для float и //
+# неточности при переводе из одной сист. счисл в другую
