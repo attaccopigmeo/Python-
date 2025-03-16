a14a3ff2c0abcee186da11df7a375c4204670f44
attaccopigmeo <camillaschmidt@yandex.ru>
2025-03-16 20:04:52 +0300

    lab17 comment

--- Семнадцатая лаба питон/4.py
+++ Семнадцатая лаба питон/4.py
@@ -27,14 +27,17 @@ class IntListB:
     def __init__(self):
         self.current = None
         self.barrier = Node(None)
+        # барьерный элемент считается следующим после последнего и предыдущим перед первым
 
     def push_front(self, new_data):  
         new_node = Node(new_data)
         new_node.prev = self.barrier
-        if self.barrier.prev is not None:
+        if self.barrier.prev is not None: # добавляемый элемент НЕ первый
+            # прежний первый элемент идёт после нового
             new_node.next = self.barrier.next
             new_node.next.prev = new_node
-        else:
+        else: # добавляемый элемент первый
+            # новый элемент оказывается и последним
             new_node.next = self.barrier
             self.barrier.prev = new_node
         self.barrier.next = new_node
@@ -43,23 +46,25 @@ class IntListB:
     def push_back(self, new_data):
         new_node = Node(new_data)
         new_node.next = self.barrier
-        if self.barrier.next is not None:
+        if self.barrier.next is not None: # добавляемый элемент НЕ первый
+            # прежний последний элемент идёт перед новым
             new_node.prev = self.barrier.prev
             new_node.prev.next = new_node
-        else:
+        else: # добавляемый элемент первый
+            # новый элемент оказывается и первым
             new_node.prev = self.barrier
             self.barrier.next = new_node
         self.barrier.prev = new_node
 
     def to_first(self):
-        self.current = self.barrier.next
+        self.current = self.barrier.next # первый элемент следует за барьерным
     
     def to_next(self):
         if self.current != self.barrier:
             self.current = self.current.next
 
     def set_data(self, d):
-        if self.current != self.barrier:
+        if self.current != self.barrier: # барьерному элементу значение не присваивается
             self.current.data = d
     
     def is_barrier(self):
@@ -77,7 +82,7 @@ def printlist(lst):
     while not lst.is_barrier():
         print(lst.current.data, end=' ')
         lst.to_next()
-    print()
+    print() # переходим на новую строку
 
 if __name__ == '__main__':
     n = int(input("Введете размер списка: "))
@@ -87,6 +92,8 @@ if __name__ == '__main__':
     lst.to_first()
     while not lst.is_barrier():
         lst.set_data(0)
+        # переходим на следующий элемент дважды - то есть, перейдём на 2 элемента вперёд
+        # тогда, т.к. начали с нечётного по номеру элемента, мы пройдём все нечётные по номеру элементы
         lst.to_next()
         lst.to_next()
     print("Новый список:")
