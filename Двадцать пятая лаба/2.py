7b87a9346a52241a9e1151c65c6ab4a745ea40fe
attaccopigmeo <camillaschmidt@yandex.ru>
2025-06-03 20:30:41 +0300

    lab 25

--- Двадцать пятая лаба/2.py
+++ Двадцать пятая лаба/2.py
@@ -3,3 +3,83 @@
 «вручную». Вычислить размер сообщения при равномерном кодировании и при кодировании с
 помощью построенного кода Хаффмана."""
 """НА ДВОРЕ ТРАВА, НА ТРАВЕ ДРОВА"""
+
+
+class Node:
+    def __init__(self, chars, freq):
+        self.chars = chars
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+
+class HuffmanTree:
+    def __init__(self, msg):
+        self.freq = self._count_freq(msg)
+        self.root = self._make_tree()
+
+    def encode_char(self, ch):
+        encoding = ''
+        cur = self.root
+        while cur.left or cur.right:
+            if cur.left is not None and ch in cur.left.chars:
+                encoding += '0'
+                cur = cur.left
+            elif cur.left is not None and ch in cur.right.chars:
+                encoding += '1'
+                cur = cur.right
+        return encoding
+
+    def encode_text(self, text):
+        encoding = ''
+        for ch in text:
+            encoding += self.encode_char(ch)
+        return encoding
+    
+    def get_char_encodings(self):
+        encodings = {}
+        for ch in self.freq:
+            encodings[ch] = self.encode_char(ch)
+        return encodings
+
+    def _count_freq(self, msg):
+        self.freq = {}
+        for ch in msg:
+            if ch in self.freq:
+                self.freq[ch] += 1
+            else:
+                self.freq[ch] = 1
+        return self.freq
+
+    def _make_tree(self):
+        nodes = []
+        for ch in self.freq:
+            nodes.append(Node(ch, self.freq[ch]))
+        while len(nodes) > 1:
+            nodes.sort(key=lambda x: x.freq)
+            node1 = nodes.pop(0)
+            node2 = nodes.pop(0)
+            new_node = Node(node1.chars + node2.chars, node1.freq + node2.freq)
+            new_node.left = node1
+            new_node.right = node2
+            nodes.append(new_node)
+        return nodes[0]
+
+
+def uniform_encoding_len(msg):
+    chars = {ch for ch in msg}
+    m = 0
+    while 2 ** m < len(chars):
+        m += 1
+    return m * len(msg)
+
+
+if __name__ == '__main__':
+    msg = input("Введите сообщение: ")
+    tree = HuffmanTree(msg)
+    print('Коды символов:')
+    for ch, code in tree.get_char_encodings().items():
+        print(ch, '=>', code)
+    print('Закодированное сообщение:', tree.encode_text(msg))
+    print('Длина сообщения при кодировании по Хаффману:', len(tree.encode_text(msg)))
+    print('Длина сообщения при равномерном кодировании:', uniform_encoding_len(msg))
--- Двадцать пятая лаба/3.py
+++ Двадцать пятая лаба/3.py
@@ -1,2 +1,33 @@
 """Написать программу для шифрования и дешифрования последовательности символов
 столбчатым шифром транспонирования. Должна быть возможность задания ключа пользователем"""
+
+
+class ColumnarTranspositionCipher:
+    def __init__(self, keyword):
+        self.key = sorted([(ch, i) for i, ch in enumerate(keyword.upper())])
+
+    def encode(self, text):
+        cipher = ''
+        for _, pos in self.key:
+            for i in range(pos, len(text), len(self.key)):
+                cipher += text[i]
+        return cipher
+    
+    def decode(self, cipher):
+        table = ['\0'] * len(cipher)
+        j = 0
+        for _, pos in self.key:
+            for i in range(pos, len(cipher), len(self.key)):
+                table[i] = cipher[j]
+                j += 1
+        return ''.join(table)
+
+
+if __name__ == '__main__':
+    keyword = input('Введите кодовое слово: ')
+    code = ColumnarTranspositionCipher(keyword)
+    msg = input('Введите сообщение: ')
+    cipher = code.encode(msg)
+    print('Закодированное сообщение:', cipher)
+    restored = code.decode(cipher)
+    print('Раскодированное сообщение:', restored)
